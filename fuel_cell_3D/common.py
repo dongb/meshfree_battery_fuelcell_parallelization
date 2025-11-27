222f69a27017f13f95e5f810bd05105103f73e7e
Bo Dong <bod@nvidia.com>
2025-11-27 12:47:20 -0800

    Enable legate/cupynumeric support and fix missing diags import
    - Re-enable try/except block for legate/cupynumeric imports
    - Add diags to scipy.sparse imports to fix test failures in mechanical_stiffness_matrix
    - Fixes AttributeError: module 'common' has no attribute 'diags'
    - All 84 tests now passing

--- fuel_cell_3D/common.py
+++ fuel_cell_3D/common.py
@@ -1,25 +1,23 @@
-#try:
+try:
     # print("Forcing use of NumPy backend by rasising a false error")
     # raise ImportError  # Force use of regular NumPy instead of legate
-    #import cupynumeric as np
-    #import legate_sparse as sparse
-    #import legate_sparse.linalg as linalg
-    #from legate.timing import time
-    #from legate_sparse import csr_array
-    #from legate_sparse.linalg import spsolve
+    import cupynumeric as np
+    import legate_sparse as sparse
+    import legate_sparse.linalg as linalg
+    from legate.timing import time
+    from legate_sparse import csr_array
+    from legate_sparse.linalg import spsolve
 
-    #use_legate = True
-    #print(f"Using legate")
-#except (RuntimeError, ImportError):
-if True:
+    use_legate = True
+    print(f"Using legate")
+except (RuntimeError, ImportError):
     from time import perf_counter_ns
 
     import numpy as np
     import scipy.sparse as sparse
     import scipy.sparse.linalg as linalg
-    from scipy.sparse import csr_array
+    from scipy.sparse import csr_array, csc_matrix, csr_matrix, bmat, block_diag, vstack, diags
     from scipy.sparse.linalg import spsolve
-    from s
 
     def time():
         return perf_counter_ns() / 1000.0
